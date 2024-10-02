f5f9dd6868aec621e5f574dae949a422251c9438
davidnguyennq <nguyennhatquan.dev@gmail.com>
2024-10-02 07:40:53 +0700

    add frmKhachHang - frmTrangChu

--- BaoHiemNhanTho/BaoHiemNhanTho/Program.cs
+++ BaoHiemNhanTho/BaoHiemNhanTho/Program.cs
@@ -16,7 +16,7 @@ namespace BaoHiemNhanTho
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmKhachHang());
         }
     }
 }
---
+++ BaoHiemNhanTho/BaoHiemNhanTho/frmKhachHang.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace BaoHiemNhanTho
+{
+    partial class frmKhachHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDSKhachHang = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDSKhachHang)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDSKhachHang
+            // 
+            this.dgvDSKhachHang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDSKhachHang.Location = new System.Drawing.Point(206, 202);
+            this.dgvDSKhachHang.Name = "dgvDSKhachHang";
+            this.dgvDSKhachHang.RowHeadersWidth = 51;
+            this.dgvDSKhachHang.RowTemplate.Height = 24;
+            this.dgvDSKhachHang.Size = new System.Drawing.Size(240, 150);
+            this.dgvDSKhachHang.TabIndex = 0;
+            // 
+            // frmKhachHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvDSKhachHang);
+            this.Name = "frmKhachHang";
+            this.Text = "frmKhachHang";
+            this.Load += new System.EventHandler(this.frmKhachHang_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDSKhachHang)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDSKhachHang;
+    }
+}
